810476f11a664d5aaa039acc16f432b60a32bb60
stamaimer <stamaimer@gmail.com>
2016-10-21 17:32:45 +0800

    restructure and and run script

---
+++ run.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+"""
+
+    stamaimer 10/21/16
+
+"""
+
+from glob import glob
+from subprocess import call
+
+for image in glob("images/*.jpg"):
+
+    print image
+
+    call(["python", "tubuledetect.py", image, "5", "5", "5"])
